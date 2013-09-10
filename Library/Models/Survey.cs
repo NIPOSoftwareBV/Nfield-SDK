43342b51a1206f34d65bc962f84465653ed3df55
mvdr <m.ruijt@niposoftware.com>
2013-09-10 11:48:29 +0200

    Introduction of NfieldSurveysService

---
+++ Library/Models/Survey.cs
@@ -0,0 +1,62 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using Newtonsoft.Json;
+
+namespace Nfield.Models
+{
+    /// <summary>
+    /// Holds all properties of a survey
+    /// </summary>
+    public class Survey
+    {
+        /// <summary>
+        /// Survey ID
+        /// </summary>
+        [JsonProperty]
+        public string SurveyId { get; internal set; }
+
+        /// <summary>
+        /// Name of the survey
+        /// </summary>
+        public string SurveyName { get; set; }
+
+        /// <summary>
+        /// The name of the client to which the survey belongs 
+        /// </summary>
+        public string ClientName { get; set; }
+
+        /// <summary>
+        /// The description of the survey
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Maximum number of successful interviews that can be done for the survey
+        /// </summary>
+        public int? Target { get; set; }
+
+        /// <summary>
+        /// original ODIN script
+        /// </summary>
+        public string Script { get; set; }
+
+        /// <summary>
+        /// Hierarchical Quota structure of the Survey for CAPI.
+        /// This data is always represented with JSON
+        /// </summary>
+        public string CapiQuota { get; set; }
+    }
+}
---
+++ Tests/Services/NfieldSurveysServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Newtonsoft.Json;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.Services.Implementation;
+using Xunit;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Tests for <see cref="NfieldSurveysService"/>
+    /// </summary>
+    public class NfieldSurveysServiceTests
+    {
+        private const string ServiceAddress = @"http://localhost/nfieldapi";
+
+        #region QueryAsync
+
+        [Fact]
+        public void TestQueryAsync_ServerReturnsQuery_ReturnsListWithSurveys()
+        {
+            const HttpStatusCode httpStatusCode = HttpStatusCode.NotFound;
+            var expectedSurveys = new Survey[]
+            { new Survey { SurveyId = "TestSurvey" },
+              new Survey { SurveyId = "AnotherTestSurvey" }
+            };
+            var mockedNfieldConnection = new Mock<INfieldConnectionClient>();
+            var mockedHttpClient = CreateHttpClientMock(httpStatusCode);
+            mockedNfieldConnection
+                .SetupGet(connection => connection.Client)
+                .Returns(mockedHttpClient.Object);
+            mockedNfieldConnection
+                .SetupGet(connection => connection.NfieldServerUri)
+                .Returns(new Uri(ServiceAddress));
+            mockedHttpClient
+                .Setup(client => client.GetAsync(ServiceAddress + @"/surveys"))
+                .Returns(CreateTask(httpStatusCode, new StringContent(JsonConvert.SerializeObject(expectedSurveys))));
+
+            var target = new NfieldSurveysService();
+            target.InitializeNfieldConnection(mockedNfieldConnection.Object);
+
+            var actualSurveys = target.QueryAsync().Result;
+
+            Assert.Equal(expectedSurveys[0].SurveyId, actualSurveys.ToArray()[0].SurveyId);
+            Assert.Equal(expectedSurveys[1].SurveyId, actualSurveys.ToArray()[1].SurveyId);
+            Assert.Equal(2, actualSurveys.Count());
+        }
+
+        #endregion
+
+        private Task<HttpResponseMessage> CreateTask(HttpStatusCode httpStatusCode, HttpContent content = null)
+        {
+            return Task.Factory.StartNew(() => new HttpResponseMessage(httpStatusCode) { Content = content });
+        }
+
+        private Mock<INfieldHttpClient> CreateHttpClientMock(HttpStatusCode httpStatusCode)
+        {
+            var mockedHttpClient = new Mock<INfieldHttpClient>();
+
+            //setup the mocked HttpClient to return httpStatusCode for all methods that send a request to the server
+
+            mockedHttpClient
+                .Setup(client => client.GetAsync(It.IsAny<string>()))
+                .Returns(CreateTask(httpStatusCode));
+
+            return mockedHttpClient;
+        }
+
+    }
+}
