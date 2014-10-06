a0eb3d9c17c5ff88c4c512d8f1d87bdcfdd39ece
m.hafi <m.hafi@niposoftware.com>
2014-10-06 12:17:33 +0200

    Publish Survey features are implemented

---
+++ Library/Services/Implementation/NfieldPublishSurveyService.cs
@@ -0,0 +1,89 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nfield.Extensions;
+using Nfield.Infrastructure;
+using Nfield.Models;
+
+namespace Nfield.Services.Implementation
+{
+    /// <summary>
+    /// Implementation of <see cref="INfieldPublishSurveyService"/>
+    /// </summary>
+    internal class NfieldPublishSurveyService : INfieldPublishSurveyService, INfieldConnectionClientObject
+    {
+        public Task<PackagePublishState> GetAsync(string surveyId)
+        {
+            CheckSurveyId(surveyId);
+
+
+            return Client.GetAsync(PublishSurveyApi(surveyId).AbsoluteUri)
+                .ContinueWith(
+                    responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+                .ContinueWith(
+                    stringTask =>
+                        JsonConvert.DeserializeObject<PackagePublishState>(stringTask.Result))
+                .FlattenExceptions();
+        }
+
+        public Task PutAsync(string surveyId)
+        {
+            CheckSurveyId(surveyId);
+
+            return
+                Client.PutAsync(PublishSurveyApi(surveyId).AbsoluteUri, new StringContent(string.Empty))
+                    .FlattenExceptions();
+        }
+
+
+        #region Implementation of INfieldConnectionClientObject
+
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        #endregion
+
+        private static void CheckSurveyId(string surveyId)
+        {
+            if (surveyId == null)
+                throw new ArgumentNullException("surveyId");
+            if (surveyId.Trim().Length == 0)
+                throw new ArgumentException("surveyId cannot be empty");
+        }
+
+       
+
+        private INfieldHttpClient Client
+        {
+            get { return ConnectionClient.Client; }
+        }
+
+        private Uri PublishSurveyApi(string surveyId)
+        {
+            var uriText = new StringBuilder(ConnectionClient.NfieldServerUri.AbsoluteUri);
+            uriText.AppendFormat("Surveys/{0}/Publish", surveyId);
+            return new Uri(uriText.ToString());
+        }
+    }
+}
---
+++ Tests/Services/NfieldPublishSurveyServiceTests.cs
@@ -0,0 +1,119 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using Moq;
+using Nfield.Infrastructure;
+using Nfield.Services.Implementation;
+using Xunit;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Tests for <see cref="NfieldSurveyScriptService"/>
+    /// </summary>
+    public class NfieldPublishSurveyServiceTests : NfieldServiceTestsBase
+    {
+        private readonly NfieldPublishSurveyService _target;
+        readonly Mock<INfieldHttpClient> _mockedHttpClient;
+        readonly Mock<INfieldConnectionClient> _mockedNfieldConnection;
+
+        public NfieldPublishSurveyServiceTests()
+        {
+            _mockedNfieldConnection = new Mock<INfieldConnectionClient>();
+            _mockedHttpClient = CreateHttpClientMock(_mockedNfieldConnection);
+
+            _target = new NfieldPublishSurveyService();
+            _target.InitializeNfieldConnection(_mockedNfieldConnection.Object);
+
+        }
+
+        #region GetStatusAsync
+
+        [Fact]
+        public void TestGetAsync_WhenSurveyIdIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => UnwrapAggregateException(_target.GetAsync(null)));
+        }
+
+        [Fact]
+        public void TestGetAsync_WhenSurveyIdIsEmptyString_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => UnwrapAggregateException(_target.GetAsync(string.Empty)));
+        }
+
+        [Fact]
+        public void TestGetAsync_WhenSurveyIdIsWhiteSpace_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => UnwrapAggregateException(_target.GetAsync(" ")));
+        }
+
+        [Fact]
+        public void TestGetAsync_Always_CallsCorrectURI()
+        {
+            const string surveyId = "SurveyId";
+            
+            _mockedHttpClient
+                .Setup(client => client.GetAsync(ServiceAddress + "Surveys/" + surveyId + "/Publish"))
+                .Returns(CreateTask(HttpStatusCode.OK, new ObjectContent<int>(0, new JsonMediaTypeFormatter())));
+
+            var actual = _target.GetAsync(surveyId).Result;
+
+            Assert.Equal(0, (int) actual);
+        }
+
+        #endregion
+
+        #region StartFieldworkAsync
+
+        [Fact]
+        public void TestPutAsync_WhenSurveyIdIsNull_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => UnwrapAggregateException(_target.PutAsync(null)));
+        }
+
+        [Fact]
+        public void TestPutAsync_WhenSurveyIdIsEmptyString_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => UnwrapAggregateException(_target.PutAsync(string.Empty)));
+        }
+
+        [Fact]
+        public void TestPutAsync_WhenSurveyIdIsWhiteSpace_Throws()
+        {
+            Assert.Throws<ArgumentException>(() => UnwrapAggregateException(_target.PutAsync(" ")));
+        }
+
+        [Fact]
+        public void TestPutAsync_Always_CallsCorrectURI()
+        {
+            const string surveyId = "SurveyId";
+            _mockedHttpClient.Setup(c => c.PutAsync(It.IsAny<string>(), It.IsAny<HttpContent>()))
+                .Returns(CreateTask(HttpStatusCode.OK, new StringContent(string.Empty)));
+
+            _target.PutAsync(surveyId);
+
+            _mockedHttpClient
+                .Verify(client => client.PutAsync(ServiceAddress + "Surveys/" + surveyId + "/Publish", It.IsAny<HttpContent>()), Times.Once());
+        }
+
+        #endregion
+
+       
+    }
+}
