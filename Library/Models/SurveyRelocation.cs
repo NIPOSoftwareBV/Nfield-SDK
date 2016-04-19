aa98502c503ddbc98d253ef29b43c58357cf6885
Hugo <h.sauniere@outlook.es>
2016-04-19 10:15:38 +0200

    Implementation of the SurveyRelocations and SurveyPublicIds services

---
+++ Library/Models/SurveyRelocation.cs
@@ -0,0 +1,33 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Nfield.Models
+{
+    /// <summary>
+    /// Holds a survey relocation
+    /// </summary>
+    public class SurveyRelocation
+    {
+        /// <summary>
+        /// The end reason
+        /// </summary>
+        public string Reason { get; set; }
+
+        /// <summary>
+        /// The url to relocate to
+        /// </summary>
+        public string Url { get; set; }
+    }
+}
