4ca11d059c9b9f1b2f77aba3475809ba8d2fc6c6
marcoNipo <m.bergantin@nipo.com>
2017-08-08 11:35:31 +0200

    Add Invitation Image

---
+++ Library/Services/Implementation/NfieldSurveyInvitationImagesService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nfield.Extensions;
+using Nfield.Infrastructure;
+using Nfield.Models;
+
+namespace Nfield.Services.Implementation
+{
+    internal class NfieldSurveyInvitationImagesService : INfieldSurveyInvitationImagesService, INfieldConnectionClientObject
+    {
+        public Task<AddInvitationImageResult> AddImage(string surveyId, string filename, Stream content)
+        {
+            CheckRequiredStringArgument(surveyId, nameof(surveyId));
+            CheckRequiredStringArgument(filename, nameof(filename));
+            CheckRequiredArgument(content, nameof(content));
+
+            var uri = SurveyInvitationImagesTemplatesUrl(surveyId, filename);
+            var imageContent = new StreamContent(content);
+            return Client.PostAsync(uri, imageContent)
+                    .ContinueWith(task => JsonConvert.DeserializeObject<AddInvitationImageResult>(
+                        task.Result.Content.ReadAsStringAsync().Result))
+                    .FlattenExceptions();
+        }
+
+        private INfieldHttpClient Client => ConnectionClient.Client;
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        private string SurveyInvitationImagesTemplatesUrl(string surveyId, string filename)
+        {
+            var result = new StringBuilder(ConnectionClient.NfieldServerUri.AbsoluteUri);
+            result.AppendFormat(CultureInfo.InvariantCulture, @"Surveys/{0}/InvitationImages/{1}", surveyId, filename);
+            return result.ToString();
+        }
+
+        private static void CheckRequiredStringArgument(string argument, string name)
+        {
+            CheckRequiredArgument(argument, name);
+            if (argument.Trim().Length == 0)
+                throw new ArgumentException($"{name} cannot be empty");
+        }
+
+        private static void CheckRequiredArgument(object argument, string name)
+        {
+            if (argument == null)
+                throw new ArgumentNullException(name);
+        }
+    }
+}
---
+++ Tests/Services/NfieldSurveyInvitationImagesServiceTests.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Security.Policy;
+using System.Text;
+using Moq;
+using Newtonsoft.Json;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.Services.Implementation;
+using Xunit;
+
+namespace Nfield.Services
+{
+    public class NfieldSurveyInvitationImagesServiceTests : NfieldServiceTestsBase
+    {
+        #region AddImage parameters validation
+
+        [Fact]
+        public void TestAddImage_SurveyIsNull_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentNullException>(() =>
+                UnwrapAggregateException(target.AddImage(null, "somefilename", new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_SurveyIsEmpty_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentException>(() =>
+                UnwrapAggregateException(target.AddImage(string.Empty, "somefilename", new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_SurveyIsWhitespace_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentException>(() =>
+                UnwrapAggregateException(target.AddImage("   ", "somefilename", new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_FilenameIsNull_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentNullException>(() =>
+                UnwrapAggregateException(target.AddImage("survey", null, new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_FilenameIsEmpty_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentException>(() =>
+                UnwrapAggregateException(target.AddImage("survey", string.Empty, new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_FilenameIsWhitespace_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentException>(() =>
+                UnwrapAggregateException(target.AddImage("survey", "   ", new MemoryStream())));
+        }
+
+        [Fact]
+        public void TestAddImage_ContentIsNull_Throws()
+        {
+            var target = new NfieldSurveyInvitationImagesService();
+
+            Assert.Throws<ArgumentNullException>(() =>
+                UnwrapAggregateException(target.AddImage("survey", "somefilename", null)));
+        }
+
+        #endregion
+
+        [Fact]
+        public void TestAddImage_AllParamsOk_Successful()
+        {
+            const string surveyId = "surveyWithImages";
+            const string filename = "imageForSurvey";
+
+            var target = new NfieldSurveyInvitationImagesService();
+            var mockClient = InitMockClient(surveyId, filename);
+            target.InitializeNfieldConnection(mockClient);
+
+            AddInvitationImageResult actual;
+            using (var stream = new MemoryStream(new byte[] {0, 1, 2, 3, 4, 5, 6, 7}))
+            {           
+                actual = target.AddImage(surveyId, filename, stream).Result;
+            }
+
+            Assert.NotEmpty(actual.Link);
+            Assert.True(actual.Link.Contains(surveyId));
+            Assert.True(actual.Link.Contains(filename));
+        }
+
+        private INfieldConnectionClient InitMockClient(string surveyId, string filename)
+        {
+            var expectedResponseContent = new AddInvitationImageResult
+            {
+                Link = @"testResultLink.blobstorage/" + surveyId + "/" + filename
+            };
+
+            var mockedNfieldConnection = new Mock<INfieldConnectionClient>();
+            var mockedHttpClient = CreateHttpClientMock(mockedNfieldConnection);
+            var responseContentString = JsonConvert.SerializeObject(expectedResponseContent);
+            var responseContent = new StringContent(responseContentString, Encoding.Unicode, "application/json");
+            var uri = ServiceAddress + "Surveys/" + surveyId + "/InvitationImages/" + filename;
+            mockedHttpClient.Setup(client => client.PostAsync(It.Is<string>(s => s == uri), 
+                                It.IsAny<StreamContent>()))
+                            .Returns(CreateTask(HttpStatusCode.OK, responseContent));
+
+            return mockedNfieldConnection.Object;
+        }
+    }
+}
