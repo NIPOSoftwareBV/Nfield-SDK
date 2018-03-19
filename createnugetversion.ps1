cfb105f1b2cbacb12c023cc6e46a79cd6262c365
Leonard Stronkhorst <l.stronkhorst@nipo.com>
2018-03-19 15:18:54 +0100

    add powershell which creates nuget version

---
+++ createnugetversion.ps1
@@ -0,0 +1,28 @@
+# Create NuGet Version
+# The following version format will be used: Major.Minor.Patch
+# The segment Major and Minor should be configured in version.txt (see root of Nfield-SDK repo)
+# The 3rd segment (Patch) will be determined by {buildId}{suffix}
+if( -Not (Test-Path version.txt))
+{
+    Write-Error "version.txt does not exist"
+    exit 1	
+}
+$VersionFormat = Get-Content version.txt | Where-Object { !$_.StartsWith("#") }
+Write-Host "VersionFormat:" $VersionFormat
+if(([regex]::Matches($VersionFormat, "\." )).count -ne 2) 
+{
+    Write-Error "Version is in format $VersionFormat but should be in format 0.1.{buildId}{suffix}"
+    exit 1	
+}
+$BuildId = $env:BUILD_BUILDID
+Write-Host "BuildId:" $BuildId
+$Branch = $env:BUILD_SOURCEBRANCHNAME
+Write-Host "Branch:" $Branch
+$BuildReason = $env:BUILD_REASON
+Write-Host "BuildReason:" $BuildReason
+# if BuildReason == remove prelease tag, then $Suffix = ""
+$Suffix = if ($Branch -eq "master") {"-beta"} else {"-alpha"}
+Write-Host "Suffix:" $Suffix
+$Version = $VersionFormat.replace("{buildId}",$BuildId).replace("{suffix}",$Suffix)
+Write-Host "##vso[task.setvariable variable=Version]$Version"
+Write-Host "Version:" $Version
