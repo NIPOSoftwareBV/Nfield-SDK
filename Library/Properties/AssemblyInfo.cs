1134aa718a6a9dbfaec3f39e11588451ef073842
Marco Bergantin <m.bergantin@nipo.com>
2018-03-28 13:32:43 +0200

    Remove nuspec, move metadata to prj file

--- Library/Properties/AssemblyInfo.cs
+++ Library/Properties/AssemblyInfo.cs
@@ -18,7 +18,6 @@ using System.Runtime.CompilerServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("NIPO Software Nfield SDK for Windows")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
