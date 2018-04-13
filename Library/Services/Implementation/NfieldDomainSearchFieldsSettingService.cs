82fb821cb186d0bf20333ceec388fae05334519a
DFernandezNipo <d.fernandez@nipo.com>
2018-04-13 12:58:21 +0200

    wrong url Add sdk initializer

--- Library/Services/Implementation/NfieldDomainSearchFieldsSettingService.cs
+++ Library/Services/Implementation/NfieldDomainSearchFieldsSettingService.cs
@@ -58,7 +58,7 @@ namespace Nfield.Services.Implementation
         private string DomainEmailSettingsUrl()
         {
             var result = new StringBuilder(ConnectionClient.NfieldServerUri.AbsoluteUri);
-            result.AppendFormat(CultureInfo.InvariantCulture, @"SearchFieldSetting");
+            result.AppendFormat(CultureInfo.InvariantCulture, @"DomainSearchFieldsSetting");
             return result.ToString();
         }
     }
