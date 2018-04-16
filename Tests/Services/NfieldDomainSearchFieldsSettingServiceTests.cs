b04942e6ccdeb8732407821f827ff6383e3dc212
Daniel Fernandez <d.fernandez@nipo.com>
2018-04-16 12:18:37 +0200

    Fix a name in the service service tests

---
+++ Tests/Services/NfieldDomainSearchFieldsSettingServiceTests.cs
@@ -0,0 +1,85 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Nfield.Models;
+using Nfield.Services.Implementation;
+using Xunit;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Tests for <see cref="NfieldDomainEmailSettingsService"/>
+    /// </summary>
+    public class NfieldDomainSearchFieldsSettingServiceTests : NfieldServiceTestsBase
+    {
+
+        private readonly DomainSearchFieldsSetting _expected = new DomainSearchFieldsSetting()
+        {
+            Fields = new[]
+            {
+                "field1",
+                "field3"
+            }
+        };
+
+        #region Get
+
+        [Fact]
+        public void TestGetAsync_ReturnsData()
+        {
+            
+
+            var target = new NfieldDomainSearchFieldsSettingService();
+            var mockClient = InitMockClientGet(ServiceAddress + @"DomainSearchFieldsSetting", _expected);
+            target.InitializeNfieldConnection(mockClient);
+
+
+            var actual = target.GetAsync().Result;
+            Assert.Equal(_expected.Fields,actual.Fields);
+          
+        }
+
+        #endregion
+
+        #region Put
+
+        [Fact]
+        public void TestPutAsync_SettingsNull_Throws()
+        {
+            var target = new NfieldDomainEmailSettingsService();
+            Assert.Throws<ArgumentNullException>(() =>
+                UnwrapAggregateException(target.PutAsync(null)));
+        }
+
+        [Fact]
+        public void TestPutAsync_ReturnsData()
+        {
+           
+            var target = new NfieldDomainSearchFieldsSettingService();
+            var mockClient = InitMockClientPut(ServiceAddress + @"DomainSearchFieldsSetting", _expected, _expected);
+            target.InitializeNfieldConnection(mockClient);
+
+            var actual = target.PutAsync(_expected).Result;
+            Assert.Equal(_expected.Fields, actual.Fields);
+        }
+
+
+
+        #endregion
+
+
+    }
+}
