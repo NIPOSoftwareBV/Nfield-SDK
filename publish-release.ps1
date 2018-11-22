ea1a5baa5ce3d02126cba6071381df0e03b4b516
Marco Bergantin <m.bergantin@nipo.com>
2018-11-22 07:59:16 +0100

    Improve automation in SDK release (#143)

---
+++ publish-release.ps1
@@ -0,0 +1,30 @@
+param(
+    [Parameter(Mandatory=$true)] $AccessToken
+)
+
+$organization = "NIPOSoftware"
+$repository = "Nfield-SDK"
+
+$Headers = @{
+    Authorization = 'Basic {0}' -f [System.Convert]::ToBase64String([char[]]($AccessToken));
+}
+
+$apiUrl = "https://api.github.com/repos/{0}/{1}/releases/{2}" -f  $organization, $repository, $env:releaseId
+$description = "Package version: " + $env:Version
+$patchBody = @{ 
+    prerelease = 'false';
+    body = $description
+} | ConvertTo-Json
+
+try
+{
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-RestMethod -Uri $apiUrl -Method Patch -Headers $Headers -Body $patchBody
+}
+catch
+{
+    Write-Host Error when trying to publish the release: $_
+    exit 1
+}
+
+Write-Host Release $release.name published!
