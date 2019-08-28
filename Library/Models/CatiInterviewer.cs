0ffd638996d566ba1859ed27aea22f0e062c9f50
w.doze@niposoftware.com <w.doze@niposoftware.com>
2019-08-28 15:31:30 +0200

    Cat interviewers service added

---
+++ Library/Models/CatiInterviewer.cs
@@ -0,0 +1,45 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using Newtonsoft.Json;
+using System;
+
+namespace Nfield.Models
+{
+    public class CatiInterviewer
+    {
+        /// <summary>
+        /// Unique id of the cati interviewer
+        /// </summary>
+        [JsonProperty]
+        public string InterviewerId { get; internal set; }
+
+        /// <summary>
+        /// User name cati interviewer uses to sign in
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Password of cati interviewer, only used when creating a cati interviewer
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Last time the password of the cati interviewer was changed
+        /// </summary>
+        [JsonProperty]
+        public DateTime? LastPasswordChangeTime { get; internal set; }
+    }
+}
