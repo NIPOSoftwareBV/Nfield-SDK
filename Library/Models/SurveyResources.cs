28c76203698fc7307cfbf79e180e7af4e84462bd
daniq <d.quilon@kantar.com>
2020-01-07 16:05:05 +0100

    Create SurveyResources Service

---
+++ Library/Models/SurveyResources.cs
@@ -0,0 +1,39 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Newtonsoft.Json;
+
+namespace Nfield.Models
+{
+    /// <summary>
+    /// Holds the properties of survey resources
+    /// </summary>
+    public class SurveyResources : SurveyBase
+    {
+        public DateTime? CreationDate { get; internal set; }
+
+        public string ClientName { get; internal set; }
+
+        [JsonProperty]
+        public string Owner { get; internal set; }
+
+        public DateTime? LastDataDownloadDate { get; internal set; }
+
+        public DateTime? LastDataCollectionDate { get; internal set; }
+
+        public long? Size { get; internal set; }
+    }
+}
---
+++ Library/Services/Implementation/NfieldSurveyResourcesService.cs
@@ -0,0 +1,66 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using Newtonsoft.Json;
+using Nfield.Extensions;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nfield.Services.Implementation
+{
+    /// <summary>
+    /// Implementation of <see cref="INfieldSurveyResourcesService"/>
+    /// </summary>
+    internal class NfieldSurveyResourcesService : INfieldSurveyResourcesService, INfieldConnectionClientObject
+    {
+        #region Implementation of INfieldSurveyResourcesService
+
+        private Uri SurveyResourcesApi => new Uri(ConnectionClient.NfieldServerUri, "SurveyResources/");
+
+        /// <summary>
+        /// See <see cref="INfieldSurveyResourcesService.QueryAsync"/>
+        /// </summary>
+        public Task<IQueryable<SurveyResources>> QueryAsync()
+        {
+            return Client.GetAsync(SurveyResourcesApi)
+                         .ContinueWith(
+                             responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+                         .ContinueWith(
+                             stringTask =>
+                             JsonConvert.DeserializeObject<List<SurveyResources>>(stringTask.Result).AsQueryable())
+                         .FlattenExceptions();
+        }
+        
+        #endregion
+
+        #region Implementation of INfieldConnectionClientObject
+
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        #endregion
+
+        private INfieldHttpClient Client => ConnectionClient.Client;
+    }
+  
+}
