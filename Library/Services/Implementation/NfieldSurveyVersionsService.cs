829d9a84536e2c9ef09a82611b23f4ace1dc2076
Carlos Cocera Perez <c.cocera.perez@kantar.com>
2020-05-07 09:20:36 +0200

    Add service

---
+++ Library/Services/Implementation/NfieldSurveyVersionsService.cs
@@ -0,0 +1,71 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using Newtonsoft.Json;
+using Nfield.Extensions;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Nfield.Services.Implementation
+{
+    internal class NfieldSurveyVersionsService : INfieldSurveyVersionsService, INfieldConnectionClientObject
+    {
+        #region Implementation of INfieldSurveyPackageService
+
+        public System.Threading.Tasks.Task<IEnumerable<SurveyVersion>> GetSurveyPackageAsync(string surveyId)
+        {
+            ValidateParams(surveyId);
+
+            return Client.GetAsync(SurveyVersionsApi(surveyId))
+                         .ContinueWith(task => task.Result.Content.ReadAsStringAsync().Result)
+                         .ContinueWith(task => JsonConvert.DeserializeObject<IEnumerable<SurveyVersion>>(task.Result))
+                         .FlattenExceptions();
+        }
+
+        #endregion
+
+        #region Implementation of INfieldConnectionClientObject
+
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        #endregion
+
+        private INfieldHttpClient Client
+        {
+            get { return ConnectionClient.Client; }
+        }
+
+        private Uri SurveyVersionsApi(string surveyId)
+        {
+            return new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{surveyId}/Versions");
+        }
+
+        private static void ValidateParams(string surveyId)
+        {
+            if (surveyId == null)
+                throw new ArgumentNullException("surveyId");
+
+            if (surveyId.Trim().Length == 0)
+                throw new ArgumentException("surveyId cannot be empty");
+        }
+    }
+}
