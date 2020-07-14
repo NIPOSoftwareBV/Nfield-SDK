6428a1260cc2fc5aa211d4ca49f9444584a6df8c
Behnaz <b.beaumont@nipo.com>
2020-07-14 12:45:23 +0200

    add quota service

---
+++ Library/Services/Implementation/NfieldQuotaService.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.Services;
+using Nfield.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nfield.SDK.Services.Implementation
+{
+    internal class NfieldQuotaService : INfieldQuotaService, INfieldConnectionClientObject
+    {
+        #region Implementation of INfieldConnectionClientObject
+
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        #endregion
+
+        #region Implementation of INfieldQuotaService
+
+        public Task<IEnumerable<QuotaFrameVersion>> GetQuotaFrameVersionsAsync(string surveyId)
+        {
+            ValidateParams(surveyId);
+
+            return Client.GetAsync(QuotaFrameVersionsApi(surveyId))
+                         .ContinueWith(task => task.Result.Content.ReadAsStringAsync().Result)
+                         .ContinueWith(task => JsonConvert.DeserializeObject<IEnumerable<QuotaFrameVersion>>(task.Result))
+                         .FlattenExceptions();
+        }
+
+        #endregion
+        private INfieldHttpClient Client
+        {
+            get { return ConnectionClient.Client; }
+        }
+
+        private Uri QuotaFrameVersionsApi(string surveyId)
+        {
+            return new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{surveyId}/QuotaVersions");
+        }
+
+        private static void ValidateParams(string surveyId)
+        {
+            if (surveyId == null)
+                throw new ArgumentNullException(nameof(surveyId));
+
+            if (surveyId.Trim().Length == 0)
+                throw new ArgumentException("surveyId cannot be empty");
+        }
+    }
+}
