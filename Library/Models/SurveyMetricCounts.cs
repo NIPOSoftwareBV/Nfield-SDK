5ff28c0e6c40a5b47fbda9f5a010e36662ce706b
basbakkernipo <46323665+basbakkernipo@users.noreply.github.com>
2021-07-07 16:32:38 +0200

    [Story] Expose survey metrics through public api - AB#90702 (#232)

---
+++ Library/Models/SurveyMetricCounts.cs
@@ -0,0 +1,41 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace Nfield.Models
+{
+    public class SurveyMetricCounts
+    {
+        /// <summary>
+        /// The name of the metric.
+        /// 
+        /// The possible metric names are:
+        /// - Page Complexity
+        /// - Expression Complexity
+        /// - Interview State Size
+        /// - Expensive Command Count
+        /// </summary>
+        public string MetricName { get; set; }
+
+        /// <summary>
+        /// The number of times an interview reached the warn or block threshold for the current published version of the survey, for the specified metric
+        /// </summary>
+        public MetricCounts All { get; set; }
+
+        /// <summary>
+        /// The number of times an interview reached the warn or block threshold for the current published version of the survey, for the specified metric
+        /// </summary>
+        public MetricCounts Published { get; set; }
+    }
+}
---
+++ Library/Services/INfieldSurveyPerformanceService.cs
@@ -0,0 +1,48 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using Nfield.Models;
+using System.Threading.Tasks;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Represents a set of methods to read survey performance metrics.
+    /// </summary>
+    public interface INfieldSurveyPerformanceService
+    {
+        /// <summary>
+        /// Get the survey performance metrics, for the live interviews, that indicate how many times the survey has reached the warn and block thresholds of their respective metric.
+        /// </summary>
+        /// <param name="surveyId">The id of the survey</param>
+        /// <returns>The survey metrics containing the amount of times the survey reached the thresholds of warn and block</returns>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Exceptions.NfieldHttpResponseException"></exception>
+        Task<SurveyMetrics> GetLiveMetricsAsync(string surveyId);
+
+        /// <summary>
+        /// Get the survey performance metrics, for the test inteviews, that indicate how many times the survey has reached the warn and block thresholds of their respective metric.
+        /// </summary>
+        /// <param name="surveyId">The id of the survey</param>
+        /// <returns>The survey metrics containing the amount of times the survey reached the thresholds of warn and block</returns>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Exceptions.NfieldHttpResponseException"></exception>
+        Task<SurveyMetrics> GetTestMetricsAsync(string surveyId);
+    }
+}
