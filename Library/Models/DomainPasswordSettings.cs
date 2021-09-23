98915812c703dde12d453141ee6f648815276301
hcsauniere <h.sauniere@kantar.com>
2021-09-23 14:27:35 +0200

    Nullable properties & Postman update

--- Library/Models/DomainPasswordSettings.cs
+++ Library/Models/DomainPasswordSettings.cs
@@ -8,32 +8,32 @@
         /// <summary>
         /// Name of the password setting that determines age warning threshold for a domain        
         /// </summary>
-        public int AgeWarnThreshold { get; set; }
+        public int? AgeWarnThreshold { get; set; }
 
         /// <summary>
         /// Name of the password setting that determines enforce two factor authentication is enabled for a domain
         /// </summary>
-        public bool EnforceTwoFactorAuthentication { get; set; }
+        public bool? EnforceTwoFactorAuthentication { get; set; }
 
         /// <summary>
         /// Name of the password setting that determines maximum password age for a domain
         /// </summary>
-        public int MaxPasswordAge { get; set; }
+        public int? MaxPasswordAge { get; set; }
 
         /// <summary>
         /// Name of the password setting that determines minimum charsets in password for a domain
         /// </summary>
-        public int MinCharsetsInPassword { get; set; }
+        public int? MinCharsetsInPassword { get; set; }
 
         /// <summary>
         /// Name of the password setting that determines minimum password length for a domain
         /// </summary>
-        public int MinPasswordLength { get; set; }
+        public int? MinPasswordLength { get; set; }
 
         /// <summary>
         /// Name of the password setting that determines password history length for a domain
         /// </summary>
-        public int PasswordHistoryLength { get; set; }
+        public int? PasswordHistoryLength { get; set; }
 
     }
 }
