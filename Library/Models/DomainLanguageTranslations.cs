6d71fa104c4880c9f58944571713579cc3c8a4e4
Oscar Zaragoza <o.zaragoza@kantar.com>
2021-11-08 16:09:27 +0100

    DomainLanguageTranslations == SurveyLanguageTranslations

--- Library/Models/DomainLanguageTranslations.cs
+++ Library/Models/DomainLanguageTranslations.cs
@@ -1,30 +1,5 @@
 ﻿namespace Nfield.SDK.Models
 {
-    public class DomainLanguageTranslations
-    {
-        public int Id { get; set; }
-        public string InterviewButtonUnavailable { get; set; }
-        public string TooFewAnswers { get; set; }
-        public string TooManyAnswers { get; set; }
-        public string ItemAnswerRequired { get; set; }
-        public string CategoryAnswerRequired { get; set; }
-        public string AnswerRequired { get; set; }
-        public string NotInRange { get; set; }
-        public string TooManyDigitsInIntegerPart { get; set; }
-        public string LessThanMinimum { get; set; }
-        public string MoreThanMaximum { get; set; }
-        public string MustBeNumeric { get; set; }
-        public string TooManyDigitsFraction { get; set; }
-        public string DoNotUseBrowserButtonToNavigate { get; set; }
-        public string ExclusiveCategory { get; set; }
-        public string InvalidCategory { get; set; }
-        public string ButtonPause { get; set; }
-        public string ButtonStart { get; set; }
-        public string ButtonClear { get; set; }
-        public string ButtonBack { get; set; }
-        public string ButtonNext { get; set; }
-        public string Name { get; set; }
-        public string AnswerTooLong { get; set; }
-        public string DoNotUseButtonWithOtherAnswer { get; set; }
-    }
+    public class DomainLanguageTranslations : SurveyLanguageTranslations
+    { }
 }
