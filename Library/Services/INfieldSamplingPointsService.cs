ae3ac56dba97ee80d9e215e529a1dc2d634440dc
Dani F <d.fernandez@kantar.com>
2022-06-08 15:57:20 +0200

    sdk created

---
+++ Library/Services/INfieldSamplingPointsService.cs
@@ -0,0 +1,119 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nfield.Models;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Represents a set of methods to manage sampling points.
+    /// </summary>
+    public interface INfieldSamplingPointsService
+    {
+        /// <summary>
+        /// Gets all sampling points for a survey
+        /// </summary>
+        /// <param name="surveyId">The survey for which to return sampling points</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<IQueryable<SamplingPoint>> QueryAsync(string surveyId);
+
+        /// <summary>
+        /// Get a sampling point for a survey
+        /// </summary>
+        /// <param name="surveyId">The survey for which to return sampling points</param>
+        /// <param name="samplingPointId">sampling point id</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<SamplingPoint> GetAsync(string surveyId, string samplingPointId);
+
+        /// <summary>
+        /// Delete a sampling point for a survey
+        /// </summary>
+        /// <param name="surveyId">The survey for which to delete sampling point</param>
+        /// <param name="samplingPointId">sampling point id</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task RemoveAsync(string surveyId, string samplingPointId);
+
+
+        /// <summary>
+        /// Update a sampling point 
+        /// </summary>
+        /// <param name="surveyId">The survey for which to update sampling point</param>
+        /// <param name="samplingPoint">sampling point</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<SamplingPoint> UpdateAsync(string surveyId, SamplingPoint samplingPoint);
+
+        /// <summary>
+        /// Create a sampling point 
+        /// </summary>
+        /// <param name="surveyId">The survey for which to create sampling point</param>
+        /// <param name="samplingPoint">sampling point data</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<SamplingPoint> CreateAsync(string surveyId, SamplingPoint samplingPoint);
+
+        /// <summary>
+        /// Activate a sampling point 
+        /// </summary>
+        /// <param name="surveyId">The survey for which to activate sampling point</param>
+        /// <param name="samplingPointId">sampling point id</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<SamplingPoint> ActivateAsync(string surveyId, string samplingPointId);
+
+        /// <summary>
+        /// Activate sampling points 
+        /// </summary>
+        /// <param name="surveyId">The survey for which to activate sampling point</param>
+        /// <param name="samplingPointIds">sampling point ids</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task ActivateAsync(string surveyId, IEnumerable<string> samplingPointIds);
+
+        /// <summary>
+        /// Replace a sampling point 
+        /// </summary>
+        /// <param name="surveyId">The survey for which to replace sampling point</param>
+        /// <param name="samplingPointId">sampling point id</param>
+        /// <param name="samplingPoint">sampling point data</param>
+        /// <exception cref="T:System.AggregateException"></exception>
+        /// The aggregate exception can contain:
+        /// <exception cref="Nfield.Exceptions.NfieldErrorException"></exception>
+        /// <exception cref="Nfield.Exceptions.NfieldHttpResponseException"></exception>
+        Task<SamplingPoint> ReplaceAsync(string surveyId, string samplingPointId, SamplingPoint samplingPoint);
+
+
+    }
+}
--- Library/Services/Implementation/NfieldSamplingPointsService.cs
+++ Library/Services/Implementation/NfieldSamplingPointsService.cs
@@ -30,7 +30,7 @@ namespace Nfield.Services.Implementation
         #region INfieldSamplingPointsService Members
 
         /// <summary>
-        /// See <see cref="INfieldTranslationsService.QueryAsync"/>
+        /// See <see cref="INfieldSamplingPointsService.QueryAsync"/>
         /// </summary>
         public Task<IQueryable<SamplingPoint>> QueryAsync(string surveyId)
         {
@@ -43,7 +43,90 @@ namespace Nfield.Services.Implementation
                              stringTask =>
                              JsonConvert.DeserializeObject<List<SamplingPoint>>(stringTask.Result).AsQueryable())
                          .FlattenExceptions();
-        }   
+        }
+        /// <summary>
+        /// See <see cref="INfieldSamplingPointsService.GetAsync(string, string)"/>
+        /// </summary>
+        public Task<SamplingPoint> GetAsync(string surveyId, string SamplingPointId)
+        {
+            return Client.GetAsync(SamplingPointsApi(surveyId, SamplingPointId))
+                         .ContinueWith(
+                             responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+                         .ContinueWith(
+                             stringTask =>
+                             JsonConvert.DeserializeObject<SamplingPoint>(stringTask.Result))
+                         .FlattenExceptions();
+        }
+        /// <summary>
+        /// See <see cref="INfieldSamplingPointsService.RemoveAsync(string, string)"/>
+        /// </summary>
+        public Task RemoveAsync(string surveyId, string SamplingPointId)
+        {
+            return Client.DeleteAsync(SamplingPointsApi(surveyId, SamplingPointId))                         
+                         .FlattenExceptions();
+        }
+
+        /// <summary>
+        /// See <see cref="INfieldSurveysService.UpdateAsync"/>
+        /// </summary>
+        public Task<SamplingPoint> UpdateAsync(string surveyId, SamplingPoint samplingPoint)
+        {
+            if (samplingPoint?.SamplingPointId == null)
+            {
+                throw new ArgumentNullException("samplingPoint");
+            }           
+
+            return Client.PatchAsJsonAsync(SamplingPointsApi(surveyId, samplingPoint.SamplingPointId), samplingPoint)
+             .ContinueWith(
+                 responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+             .ContinueWith(
+                 stringTask => JsonConvert.DeserializeObject<SamplingPoint>(stringTask.Result))
+             .FlattenExceptions();
+        }
+
+        /// <summary>
+        /// See <see cref="INfieldSurveysService.UpdateAsync"/>
+        /// </summary>
+        public Task<SamplingPoint> CreateAsync(string surveyId, SamplingPoint samplingPoint)
+        {
+            if (samplingPoint == null)
+            {
+                throw new ArgumentNullException("samplingPoint");
+            }
+
+            return Client.PostAsJsonAsync(SamplingPointsApi(surveyId), samplingPoint)
+             .ContinueWith(
+                 responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+             .ContinueWith(
+                 stringTask => JsonConvert.DeserializeObject<SamplingPoint>(stringTask.Result))
+             .FlattenExceptions();
+        }
+
+        public Task<SamplingPoint> ActivateAsync(string surveyId, string samplingPointId)
+        {
+            return Client.PatchAsJsonAsync(SamplingPointsApi(surveyId, samplingPointId, activate:true), samplingPointId)
+            .ContinueWith(
+                responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+            .ContinueWith(
+                stringTask => JsonConvert.DeserializeObject<SamplingPoint>(stringTask.Result))
+            .FlattenExceptions();
+        }
+
+        public Task ActivateAsync(string surveyId, IEnumerable<string> samplingPointIds)
+        {
+            return Client.PatchAsJsonAsync(SamplingPointsApi(surveyId, activate:true), samplingPointIds)     
+            .FlattenExceptions();
+        }
+
+        public Task<SamplingPoint> ReplaceAsync(string surveyId, string samplingPointId, SamplingPoint samplingPoint)
+        {
+            return Client.PutAsJsonAsync(SamplingPointsApi(surveyId, samplingPointId, replace:true), samplingPointId)
+           .ContinueWith(
+               responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+           .ContinueWith(
+               stringTask => JsonConvert.DeserializeObject<SamplingPoint>(stringTask.Result))
+           .FlattenExceptions();
+        }
 
         #endregion
 
@@ -71,16 +154,20 @@ namespace Nfield.Services.Implementation
             get { return ConnectionClient.Client; }
         }
 
-        private Uri SamplingPointsApi(string surveyId, string samplingPointId = null)
+        private Uri SamplingPointsApi(string surveyId, string samplingPointId = null, bool activate = false, bool replace = false)
         {
             var path = new StringBuilder();
             path.AppendFormat("Surveys/{0}/SamplingPoints",
                     surveyId, samplingPointId);
             if (!string.IsNullOrEmpty(samplingPointId))
                 path.AppendFormat("/{0}", samplingPointId);
+            if (!activate)
+                path.AppendFormat("/Activate");
+            if (!replace)
+                path.AppendFormat("/Replace");
 
             return new Uri(ConnectionClient.NfieldServerUri, path.ToString());
         }
-
+        
     }
 }
