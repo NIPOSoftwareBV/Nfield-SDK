a219ff1def5ba64f455754ea72aac81b4a253c6a
Hugo <h.sauniere@kantar.com>
2022-06-15 13:27:39 +0200

    AB#110391 - (sdk) Add spare SP endpoints (#338)
    * Add spare SP endpoints to postman (#336)
    
    * Spare sp sdk (#337)
    
    * query
    
    * working
    
    * sdk created
    
    * changes
    
    * Update Library/Services/Implementation/NfieldSamplingPointsService.cs
    
    Co-authored-by: Hugo <h.sauniere@kantar.com>
    
    Co-authored-by: Hugo <h.sauniere@kantar.com>
    
    * update api collection variables
    
    * Yellow/sp unit tests (#339)
    
    * crud tests
    
    * tests
    
    * working
    
    * mark as obsolete
    
    * update version
    
    * fix postman
    
    * fix url
    
    * fix
    
    * 39
    
    * fix update sp
    
    * fix test
    
    * add obsolete comments
    
    Co-authored-by: Daniel Fernandez Nipo <d.fernandez@nipo.com>
    Co-authored-by: Dani F <d.fernandez@kantar.com>

--- Library/Infrastructure/NfieldSdkInitializer.cs
+++ Library/Infrastructure/NfieldSdkInitializer.cs
@@ -88,6 +88,7 @@ namespace Nfield.Infrastructure
             { typeof(INfieldThemesService), typeof(NfieldThemesService) },
             { typeof(INfieldSurveyInterviewerAssignmentQuotaLevelTargetsService), typeof(NfieldSurveyInterviewerAssignmentQuotaLevelTargetsService) },
             { typeof(INfieldCapiInterviewersService), typeof(NfieldCapiInterviewersService) },
+            { typeof(INfieldSamplingPointsService), typeof(NfieldSamplingPointsService) }
         };
 
         /// <summary>
