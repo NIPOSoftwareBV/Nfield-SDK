ca5e82cb2243d7970947f94c21d558c19ba43271
Hugo <h.sauniere@kantar.com>
2022-08-10 10:09:40 +0200

    Add new endpoints to sdk (#342)

--- Library/Services/Implementation/NfieldSurveyDataService.cs
+++ Library/Services/Implementation/NfieldSurveyDataService.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Nfield.Extensions;
 using Nfield.Infrastructure;
 using Nfield.Models;
+using Nfield.Models.NipoSoftware.Nfield.Manager.Api.Models;
 
 namespace Nfield.Services.Implementation
 {
@@ -21,12 +22,45 @@ namespace Nfield.Services.Implementation
             {
                 throw new ArgumentNullException(nameof(surveyDownloadDataRequest));
             }
-            var uri = SurveyDataUrl(surveyDownloadDataRequest.SurveyId);
+
+            var uri = new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{surveyDownloadDataRequest.SurveyId}/data");
 
             return Client.PostAsJsonAsync(uri, surveyDownloadDataRequest)
                          .ContinueWith(task => task.Result.Content.ReadAsStringAsync().Result)
                          .ContinueWith(task => JsonConvert.DeserializeObject<BackgroundTask>(task.Result))
                          .FlattenExceptions();
+        }
+
+        /// <summary>
+        /// See <see cref="INfieldSurveyDataService.PrepareDownload"/>
+        /// </summary>
+        public async Task<string> PrepareDownload(string surveyId, SurveyDataRequest surveyDataRequest)
+        {
+            CheckSurveyId(surveyId);
+
+            var uri = new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{surveyId}/DataDownload");
+
+            return await Client.PostAsJsonAsync(uri, surveyDataRequest)
+                        .ContinueWith(task => task.Result.Content.ReadAsStringAsync().Result)
+                        .ContinueWith(task => JsonConvert.DeserializeObject<BackgroundActivityStatus>(task.Result))
+                        .ContinueWith(task => ConnectionClient.GetActivityResultAsync<string>(task.Result.ActivityId, "DownloadDataUrl").Result)
+                        .FlattenExceptions().ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// See <see cref="INfieldSurveyDataService.PrepareInterviewDownload"/>
+        /// </summary>
+        public async Task<string> PrepareInterviewDownload(string surveyId, int interviewId, SurveyDataInterviewRequest surveyDataInterviewRequest)
+        {
+            CheckSurveyId(surveyId);
+
+            var uri = new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{surveyId}/DataDownload/{interviewId}");
+
+            return await Client.PostAsJsonAsync(uri, surveyDataInterviewRequest)
+                        .ContinueWith(task => task.Result.Content.ReadAsStringAsync().Result)
+                        .ContinueWith(task => JsonConvert.DeserializeObject<BackgroundActivityStatus>(task.Result))
+                        .ContinueWith(task => ConnectionClient.GetActivityResultAsync<string>(task.Result.ActivityId, "DownloadDataUrl").Result)
+                        .FlattenExceptions().ConfigureAwait(false);
         }
 
         #region Implementation of INfieldConnectionClientObject
@@ -40,15 +74,15 @@ namespace Nfield.Services.Implementation
 
         #endregion
 
-        private INfieldHttpClient Client
+        private static void CheckSurveyId(string surveyId)
         {
-            get { return ConnectionClient.Client; }
+            if (surveyId == null)
+                throw new ArgumentNullException(nameof(surveyId));
+            if (surveyId.Trim().Length == 0)
+                throw new ArgumentException("surveyId cannot be empty");
         }
 
-        private Uri SurveyDataUrl(string surveyId)
-        {
-            return new Uri(ConnectionClient.NfieldServerUri, $"surveys/{surveyId}/data");
-        }
+        private INfieldHttpClient Client => ConnectionClient.Client;
 
     }
 }
