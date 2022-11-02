255af1ca2a893cadeb47b642fd1999272fe19025
pilar <p.rodriguez.cabrero@kantar.com>
2022-11-02 15:39:02 +0100

    add missing interface

--- Library/Services/INfieldSurveySampleDataService.cs
+++ Library/Services/INfieldSurveySampleDataService.cs
@@ -27,5 +27,10 @@ namespace Nfield.Services
         /// Start a new download sample data task, gets the task
         /// </summary>
         Task<BackgroundTask> GetAsync(string surveyId, string fileName);
+
+        /// <summary>
+        /// Start a new download sample data activity, gets the activity
+        /// </summary>
+        Task<string> PrepareDownloadSampleDataAsync(string surveyId, string fileName);
     }
 }
