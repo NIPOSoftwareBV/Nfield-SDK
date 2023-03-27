1d61e03d64b03695d617220c8ed9af0f5b455443
Julio Campos <julio.campos@kantar.com>
2023-03-27 14:08:50 +0100

    Create Me Service and respective unit tests

---
+++ Library/Services/Implementation/NfieldMeService.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Nfield.Extensions;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.SDK.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nfield.SDK.Services.Implementation
+{
+    internal class NFieldMeService : INFieldMeService, INfieldConnectionClientObject
+    {
+
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+
+        public void InitializeNfieldConnection(INfieldConnectionClient connection)
+        {
+            ConnectionClient = connection;
+        }
+
+        public Task<UserRoleModel> GetUserRole()
+        {
+            return ConnectionClient.Client.GetAsync(MeApi)
+             .ContinueWith(
+                 responseMessageTask => responseMessageTask.Result.Content.ReadAsStringAsync().Result)
+             .ContinueWith(
+                 stringTask =>
+                 JsonConvert.DeserializeObject<UserRoleModel>(stringTask.Result))
+             .FlattenExceptions();
+        }
+
+        private Uri MeApi
+        {
+            get { return new Uri(ConnectionClient.NfieldServerUri, "me/role/"); }
+        }
+
+    }
+}
