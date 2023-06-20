96454d56cd35113481e4552acf5c4cb42389dad8
Alberto.Belver <alberto.belver@kantar.com>
2023-06-20 12:42:55 +0200

    PublishGitRelease.ps1 fix

--- Pipelines/Scripts/PublishGitRelease.ps1
+++ Pipelines/Scripts/PublishGitRelease.ps1
@@ -9,47 +9,47 @@ param(
     [string] $BranchName
 )
 
-Write-Host Publishing release $VersionName on GitHub
+$organization = "NIPOSoftware"
+$repository = "Nfield-SDK"
+
+Write-Host Publishing release $organization/$repository/$BranchName/$VersionName on GitHub
 Write-Host Commit Hash: $CommitHash
 Write-Host Version Name: $VersionName
 Write-Host Branch Name: $BranchName
 
 # https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#create-a-release
-
-$organization = "NIPOSoftware"
-$repository = "Nfield-SDK"
-$releaseName = $BranchName.Split("/");
-if ($releaseName.count -eq 2)
-{
-    $releaseName = $releaseName[1].Replace("-", " ")
-}
-else
-{
-    $releaseName = $releaseName[0].Replace("-", " ")
-}
-Write-Host Release Name: $releaseName
-
-$Headers = @{
-    Accept = 'application/vnd.github+json';
-    Authorization = 'Bearer {0}' -f $AccessToken;
-}
-
-$apiUrl = "https://api.github.com/repos/{0}/{1}/releases" -f  $organization, $repository,
-$description = "Package version: " + $VersionName
-
-
-$patchBody = @{ 
-    prerelease = 'true';
-    name = $releaseName;
-    body = $description;
-    tag_name = $BranchName;
-    target_commitish = $CommitHash;
-} | ConvertTo-Json
-
-Write-Host Url: $apiUrl
-
 try
 {
+
+    $releaseName = $BranchName.Split("/");
+    if ($releaseName.count -eq 2)
+    {
+        $releaseName = $releaseName[1].Replace("-", " ")
+    }
+    else
+    {
+        $releaseName = $releaseName[0].Replace("-", " ")
+    }
+    Write-Host Release Name: $releaseName
+
+    $Headers = @{
+        Accept = 'application/vnd.github+json';
+        Authorization = 'Bearer {0}' -f $AccessToken;
+    }
+
+    $apiUrl = "https://api.github.com/repos/{0}/{1}/releases" -f  $organization, $repository
+    $description = "Package version: " + $VersionName
+
+    $patchBody = @{ 
+        prerelease = 'true';
+        name = $releaseName;
+        body = $description;
+        tag_name = $BranchName;
+        target_commitish = $CommitHash;
+    } | ConvertTo-Json
+
+    Write-Host Url: $apiUrl
+
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Invoke-RestMethod -Uri $apiUrl -Method Patch -Headers $Headers -Body $patchBody
 }
@@ -59,4 +59,4 @@ catch
     exit 1
 }
 
-Write-Host Release $release.name published!
+Write-Host Release $releaseName published!
