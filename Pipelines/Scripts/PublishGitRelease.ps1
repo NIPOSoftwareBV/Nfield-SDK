1d0914d2a9485875874183c41a26ecb6ead370ea
Alberto.Belver <alberto.belver@kantar.com>
2023-06-20 09:46:58 +0200

    release test

--- Pipelines/Scripts/PublishGitRelease.ps1
+++ Pipelines/Scripts/PublishGitRelease.ps1
@@ -4,6 +4,9 @@ param(
     [Parameter(Mandatory=$true)] $Version
 )
 
+Write-Host Publishing release $Version on GitHub
+Write-Host ReleaseId: $apiUrl
+Write-Host Version: $Version
 
 $organization = "NIPOSoftware"
 $repository = "Nfield-SDK"
@@ -15,7 +18,7 @@ $Headers = @{
 $apiUrl = "https://api.github.com/repos/{0}/{1}/releases/{2}" -f  $organization, $repository, $ReleaseId
 $description = "Package version: " + $Version
 $patchBody = @{ 
-    prerelease = 'false';
+    prerelease = 'true';
     body = $description
 } | ConvertTo-Json
 
@@ -23,8 +26,8 @@ Write-Host Url: $apiUrl
 
 try
 {
-    #[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-    #Invoke-RestMethod -Uri $apiUrl -Method Patch -Headers $Headers -Body $patchBody
+    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    Invoke-RestMethod -Uri $apiUrl -Method Patch -Headers $Headers -Body $patchBody
 }
 catch
 {
