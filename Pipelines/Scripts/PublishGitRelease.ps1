8b06e84e703a516b632eabf94e8aec889c77ae7f
Alberto.Belver <alberto.belver@kantar.com>
2023-06-20 11:14:36 +0200

    improvements in the release creation

--- Pipelines/Scripts/PublishGitRelease.ps1
+++ Pipelines/Scripts/PublishGitRelease.ps1
@@ -1,25 +1,33 @@
 param(
     [Parameter(Mandatory=$true)] $AccessToken
-    [Parameter(Mandatory=$true)] $ReleaseId
-    [Parameter(Mandatory=$true)] $Version
+    [Parameter(Mandatory=$true)] $CommitHash
+    [Parameter(Mandatory=$true)] $VersionName
+    [Parameter(Mandatory=$true)] $BranchName
 )
 
-Write-Host Publishing release $Version on GitHub
-Write-Host ReleaseId: $apiUrl
-Write-Host Version: $Version
+Write-Host Publishing release $VersionName on GitHub
+Write-Host Commit Hash: $CommitHash
+Write-Host Version Name: $VersionName
+Write-Host Branch Name: $BranchName
+
+# https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#create-a-release
 
 $organization = "NIPOSoftware"
 $repository = "Nfield-SDK"
 
 $Headers = @{
-    Authorization = 'Basic {0}' -f [System.Convert]::ToBase64String([char[]]($AccessToken));
+    Accept = 'application/vnd.github+json';
+    Authorization = 'Bearer {0}' -f $AccessToken;
 }
 
-$apiUrl = "https://api.github.com/repos/{0}/{1}/releases/{2}" -f  $organization, $repository, $ReleaseId
-$description = "Package version: " + $Version
+$apiUrl = "https://api.github.com/repos/{0}/{1}/releases" -f  $organization, $repository,
+$description = "Package version: " + $VersionName
 $patchBody = @{ 
     prerelease = 'true';
-    body = $description
+    name =;
+    body = $description;
+    tag_name = $BranchName;
+    target_commitish = $CommitHash;
 } | ConvertTo-Json
 
 Write-Host Url: $apiUrl
