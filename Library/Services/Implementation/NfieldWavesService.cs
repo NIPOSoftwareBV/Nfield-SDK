379f73a83ca7615612ae8242638780f6d57ec7d4
pilar <p.rodriguez.cabrero@kantar.com>
2024-06-27 13:08:26 +0200

    waves service

---
+++ Library/Services/Implementation/NfieldWavesService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.SDK.Services;
+
+namespace Nfield.Services.Implementation
+{
+    internal class NfieldWavesService : INfieldWavesService, INfieldConnectionClientObject
+    {
+        public INfieldConnectionClient ConnectionClient { get; internal set; }
+        public void InitializeNfieldConnection(INfieldConnectionClient connection) => ConnectionClient = connection;
+
+        #region Implementation of INfieldWavesService
+
+        public async Task<IQueryable<Survey>> GetParentSurveyWaves(string parentSurveyId)
+        {
+            var uri = WavesUrl(parentSurveyId);
+
+            var response = await ConnectionClient.Client.GetAsync(uri).ConfigureAwait(false);
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<List<Survey>>(stringResponse).AsQueryable();
+        }
+
+        public async Task<Survey> AddWaveAsync(string parentSurveyId, Survey survey)
+        {
+            if (survey == null)
+                throw new ArgumentNullException(nameof(survey));
+
+            var uri = WavesUrl(parentSurveyId);
+            var response = await ConnectionClient.Client.PostAsJsonAsync(uri, survey).ConfigureAwait(false);
+
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<Survey>(stringResponse);
+        }
+
+        private Uri WavesUrl(string parentSurveyId)
+        {
+            return new Uri(ConnectionClient.NfieldServerUri, $"Surveys/{parentSurveyId}/Waves/");
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Services/NfieldWavesServiceTests.cs
@@ -0,0 +1,87 @@
+﻿//    This file is part of Nfield.SDK.
+//
+//    Nfield.SDK is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU Lesser General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    Nfield.SDK is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+
+using Moq;
+using Newtonsoft.Json;
+using Nfield.Infrastructure;
+using Nfield.Models;
+using Nfield.Services.Implementation;
+using System;
+using System.Net.Http;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+using Nfield.SDK.Models;
+using System.Linq;
+
+namespace Nfield.Services
+{
+    /// <summary>
+    /// Tests for <see cref="NfieldWavesService"/>
+    /// </summary>
+    public class NfieldWavesServiceTests : NfieldServiceTestsBase
+    {
+        [Fact]
+        public void TestWavesAsync_ServerReturnsQuery_ReturnsListWithWaves()
+        {
+            const string parentSurveyId = "parentSurveyId";
+            var expectedWavesSurveys = new[]
+            {
+                new Survey(SurveyType.OnlineBasic) { SurveyId = Guid.NewGuid().ToString() },
+                new Survey(SurveyType.OnlineBasic) { SurveyId = Guid.NewGuid().ToString(), },
+            };
+
+            var getWavesEndPoint = new Uri(ServiceAddress, $"Surveys/{parentSurveyId}/Waves/");
+
+            var mockedNfieldConnection = new Mock<INfieldConnectionClient>();
+            var mockedHttpClient = CreateHttpClientMock(mockedNfieldConnection);
+            mockedHttpClient
+                .Setup(client => client.GetAsync(getWavesEndPoint))
+                .Returns(CreateTask(HttpStatusCode.OK, new StringContent(JsonConvert.SerializeObject(expectedWavesSurveys))));
+
+            var target = new NfieldWavesService();
+            target.InitializeNfieldConnection(mockedNfieldConnection.Object);
+
+            var actualWaves = target.GetParentSurveyWaves(parentSurveyId).Result;
+
+            Assert.Equal(expectedWavesSurveys[0].SurveyId, actualWaves.ToArray()[0].SurveyId);
+            Assert.Equal(expectedWavesSurveys[1].SurveyId, actualWaves.ToArray()[1].SurveyId);
+            Assert.Equal(2, actualWaves.Count());
+        }
+
+        [Fact]
+        public void TestWaveAsync_ReturnsSurvey()
+        {
+            const string parentSurveyId = "parentSurveyId";
+            var survey = new Survey(SurveyType.OnlineBasic) { SurveyId = Guid.NewGuid().ToString() };
+
+            var getWavesEndPoint = new Uri(ServiceAddress, $"Surveys/{parentSurveyId}/Waves/");
+
+            var mockedNfieldConnection = new Mock<INfieldConnectionClient>();
+            var mockedHttpClient = CreateHttpClientMock(mockedNfieldConnection);
+            mockedHttpClient
+                .Setup(client => client.PostAsJsonAsync(getWavesEndPoint, survey))
+                .Returns(CreateTask(HttpStatusCode.OK, new StringContent(JsonConvert.SerializeObject(survey))));
+
+            var target = new NfieldWavesService();
+            target.InitializeNfieldConnection(mockedNfieldConnection.Object);
+
+            var actualSurvey = target.AddWaveAsync(parentSurveyId, survey).Result;
+
+            Assert.Equal(survey.SurveyId, actualSurvey.SurveyId);
+        }
+    }
+}
