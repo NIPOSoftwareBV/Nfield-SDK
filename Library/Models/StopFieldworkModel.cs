6c7c833baf066586f0c715869c1e9fd4abf55a29
Kirill Efishev <k.efishev@kantar.com>
2024-10-02 07:27:51 +0200

    Comment and cleanup

--- Library/Models/StopFieldworkModel.cs
+++ Library/Models/StopFieldworkModel.cs
@@ -13,8 +13,6 @@
 //    You should have received a copy of the GNU Lesser General Public License
 //    along with Nfield.SDK.  If not, see <http://www.gnu.org/licenses/>.
 
-using System;
-
 namespace Nfield.Models
 {
     /// <summary>
